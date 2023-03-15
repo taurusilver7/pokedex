1ddb10f91dfdd208f570bfc3761ce5c67a0ddddb
Devi prasad <prasad20697@gmail.com>
2023-03-15 17:06:22 +0530

    extraReducers for reducer actions created

--- src/app/slices/PokeSlice.ts
+++ src/app/slices/PokeSlice.ts
@@ -1,12 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { PokemonTypeInitialState } from "../../utils/Types";
+import { getInitalData } from "../reducers/getInitialData";
+import { getPokemonData } from "../reducers/getPokemonData";
 
-const initialState: PokemonTypeInitialState = {};
+const initialState: PokemonTypeInitialState = {
+	allPokemon: undefined,
+	randomPokemon: undefined,
+};
 
 export const PokeSlice = createSlice({
 	name: "pokemon",
 	initialState,
 	reducers: {},
+	extraReducers: (builder) => {
+		builder.addCase(getInitalData.fulfilled, (state, action) => {
+			state.allPokemon = action.payload;
+		});
+		builder.addCase(getPokemonData.fulfilled, (state, action) => {state.randomPokemon = action.payload})
+	},
 });
 
 export const {} = PokeSlice.actions;
+ 
