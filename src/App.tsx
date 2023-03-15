0bd0eca189eaf1bcb374cca115d4483eca765305
Devi prasad <prasad20697@gmail.com>
2023-03-15 10:54:19 +0530

    app routes set

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import { Background } from "./components";
 import "./scss/index.scss";
 import { Footer, Navbar, Wrapper } from "./sections";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Compare, MyList, Pokemon, Search } from "./pages";
 
 function App() {
 	return (
 		<div className="main-container">
 			<Background />
-
-			<div className="app">
-				<Navbar />
-				<Wrapper />
-				<Footer />
-			</div>
+			<BrowserRouter>
+				<div className="app">
+					<Navbar />
+					<Routes>
+						<Route path="/search" element={<Search />} />
+						<Route path="/list" element={<MyList />} />
+						<Route path="/compare" element={<Compare />} />
+						<Route path="/pokemon/:id" element={<Pokemon />} />
+						<Route element={<Navigate to="/pokemon/1" />} path="*" />
+					</Routes>
+					<Footer />
+				</div>
+			</BrowserRouter>
 		</div>
 	);
 }
