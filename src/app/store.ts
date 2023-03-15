3f67122a15fff122d849bf56b143d3f6fc8858c3
Devi prasad <prasad20697@gmail.com>
2023-03-15 10:52:19 +0530

    reducers added to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,19 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { AppSlice } from "./slices/AppSlice";
+import { PokeSlice } from "./slices/PokeSlice";
 
 export const store = configureStore({
-  reducer: {
-  },
+	reducer: {
+		app: AppSlice.reducer,
+		pokemon: PokeSlice.reducer,
+	},
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
+	ReturnType,
+	RootState,
+	unknown,
+	Action<string>
 >;
