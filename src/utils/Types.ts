1e3a56e937f2587e5d34984306e0014fc83ec4a0
Devi prasad <prasad20697@gmail.com>
2023-03-17 17:45:09 +0530

    pokemon interface type

--- src/utils/Types.ts
+++ src/utils/Types.ts
@@ -40,3 +40,35 @@ export interface pokemonInterfaceType {
 export interface userPokemonType extends generatedPokemonType {
 	firebaseId?: string;
 }
+
+export interface pokemonStatsType {
+	name: string;
+	value: string;
+}
+
+
+export type pokemonStatType =
+	| "vulnerable"
+	| "weakness"
+	| "strength"
+	| "resistance";
+
+export type pokemonElementType =
+	| "bug"
+	| "dark"
+	| "dragon"
+	| "electric"
+	| "fairy"
+	| "fighting"
+	| "fire"
+	| "flying"
+	| "ghost"
+	| "grass"
+	| "ground"
+	| "ice"
+	| "normal"
+	| "poison"
+	| "psychic"
+	| "rock"
+	| "steel"
+	| "water";
