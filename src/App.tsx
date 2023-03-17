8dcf2b4f827510aabaa979a735ba1fd246737313
Devi prasad <prasad20697@gmail.com>
2023-03-17 10:15:54 +0530

    global toast setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import { Background } from "./components";
 import "./scss/index.scss";
-import { Footer, Navbar, Wrapper } from "./sections";
+import { Footer, Navbar } from "./sections";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Compare, MyList, Pokemon, Search } from "./pages";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { useEffect } from "react";
+import { ToastContainer, toast, ToastOptions } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { clearToast } from "./app/slices/AppSlice";
 
 function App() {
+	const { toasts } = useAppSelector(({ app }) => app);
+	const dispatch = useAppDispatch();
+
+	useEffect(() => {
+		if (toasts.length) {
+			const toastOptions: ToastOptions = {
+				position: "top-right",
+				autoClose: 2000,
+				pauseOnHover: true,
+				draggable: false,
+				hideProgressBar: true,
+				type: "info",
+				theme: "dark",
+			};
+			toasts.forEach((message: string) => {
+				toast(message, toastOptions);
+			});
+			dispatch(clearToast());
+		}
+	}, [toasts, dispatch]);
+
 	return (
 		<div className="main-container">
 			<Background />
@@ -19,6 +45,7 @@ function App() {
 						<Route element={<Navigate to="/pokemon/1" />} path="*" />
 					</Routes>
 					<Footer />
+					<ToastContainer />
 				</div>
 			</BrowserRouter>
 		</div>
