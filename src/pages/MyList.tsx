1836bdb28aa821aad2e7feef9e47b0d4d28404d2
Devi prasad <prasad20697@gmail.com>
2023-03-18 12:42:43 +0530

    list page popluated with getUsrPokemon list

--- src/pages/MyList.tsx
+++ src/pages/MyList.tsx
@@ -1,15 +1,26 @@
-import React from "react";
-import { useAppSelector } from "../app/hooks";
-import { Login } from "../components";
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { getUserPokemon } from "../app/reducers/getUserPokemon";
+import { CardGrid, Login } from "../components";
 import { Wrapper } from "../sections";
 
 const MyList = () => {
 	const { userInfo } = useAppSelector(({ app }) => app);
+	const { userPokemons } = useAppSelector(({ pokemon }) => pokemon);
+	const dispatch = useAppDispatch();
+
+	useEffect(() => {
+		dispatch(getUserPokemon());
+	}, [userInfo, dispatch]);
+
+	// useEffect(() => {
+	// 	console.log(userPokemons);
+	// }, [userPokemons]);
 
 	return (
 		<div className="list">
 			{/* Login component or Pokemon grid based on the uerInfo state value */}
-			{!userInfo && <Login />}
+			{userInfo ? <CardGrid pokemons={userPokemons} /> : <Login />}
 		</div>
 	);
 };
