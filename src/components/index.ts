8f1b2e27f7b5be660e9210d14d021560be84314a
Devi prasad <prasad20697@gmail.com>
2023-03-19 00:11:04 +0530

    loader component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Background } from "./Background";
 export { default as Login } from "./Login";
+export { default as Loader } from "./Loader";
 export { default as CardGrid } from "./CardGrid";
 export { default as CompareContainer } from "./CompareContainer";
